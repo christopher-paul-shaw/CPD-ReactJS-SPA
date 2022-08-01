0e565cd2d703d885d0b0e5d8102a7e0d6c17a3ed
Christopher Shaw <chris@chris-shaw.com>
2022-08-01 12:19:55 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      
+
+      
+      <nav class="bg-gray-900 text-white p-4 drop-shadow-lg mb-10">
+        <ul class="w-full max-w-screen-md">
+          <li class="inline px-2 font-bold">
+            <span>ReactJS SPA</span>
+          </li>
+          <li class="inline px-2">
+            <a href="/" >Home</a>
+          </li>
+          <li class="inline px-2">
+            <a href="/about" >About</a>
+          </li>
+        </ul>
+      </nav>
+        {/*
+          A <Routes> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Routes> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+     
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div class="flex items-center justify-center w-full max-w-screen-md">
+      <p>Welcome to this example ReactJS Application</p>
     </div>
   );
 }
+
+function About() {
+  return (
+    <div class="flex items-center justify-center w-full max-w-screen-md">
+      <p>This is just playing around to test writing a basic Single Page Application using ReactJS</p>
+    </div>
+  );
+}
+
 
 export default App;
